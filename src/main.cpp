d7e0a74cecafd0a6a8e490e2c20549fb17b2730e
bpapesh <brent.t.papesh@gmail.com>
2019-10-14 15:51:18 +1100

    trying server

--- src/main.cpp
+++ src/main.cpp
@@ -219,24 +219,24 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
   return ESP_OK;
 }
 
-// static void initialise_wifi(void *arg)
-// {
-//   tcpip_adapter_init();
-//   ESP_ERROR_CHECK(esp_event_loop_init(event_handler, arg));
-//   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-//   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-//   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-//   wifi_config_t wifi_config = {
-//       .sta = {
-//           .ssid = WIFI_SSID,
-//           .password = WIFI_PASS,
-//       },
-//   };
-//   ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-//   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-//   ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-//   ESP_ERROR_CHECK(esp_wifi_start());
-// }
+static void initialise_wifi(void *arg)
+{
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, arg));
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  wifi_config_t wifi_config = {
+      .sta = {
+          .ssid = WIFI_SSID,
+          .password = WIFI_PASS,
+      },
+  };
+  ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
 
 void loop()
 {
